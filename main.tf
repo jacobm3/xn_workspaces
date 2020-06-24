aa148ea0e7ecfb75ceeaba681557f401fa195ffb
Jacob Martinson <jacobm3@gmail.com>
2020-06-24 18:25:40 -0500

    update `date +%Y.%m.%d-%H.%M.%S-%s`

--- main.tf
+++ main.tf
@@ -1,9 +1,14 @@
-# resource "gitlab_project" "test" {
-#     name = "xn_test"
-# }
+resource "gitlab_project" "test" {
+     name = "xn_test"
+}
 
 resource "tfe_workspace" "xn_test" {
   name         = "xn_test"
   organization = "jacobm3"
+  vcs_repo {
+    identifier         = "jacobm3/xn_test"
+  }
+
+  depends_on = gitlab_project.test
 }
 
